af720ee97470b17beec82c7c572a4f1c535fbf90
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-08-31 17:48:26 +0200

    finish task6 js

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Part1 - Login Form
+> Create login page and when user press login button create object login that hold
+ the data and if username = admin and password = 123 show him message
+ “welcome” , else show him message “not registered”
+ don’t use alert or prompt , use dom to get the data from user and display it
+ [part1 Login Form - admin](img/lab6P11.jpg)
+ [part2 Login Form - other](img/lab6P12.jpg)
+
+# part2 - Todo List
+> Create Todo list app similar to this ,
+  when user write task name and press add new task will be added to tasks list
+  When user click the done mark in green the task status will be updated to done
+  When user click the delete button , the task should be deleted from list
+ [part1 Todo List - add without enter any data](img/lab6P21.jpg)
+ [part2 Todo List - add task name and press add](img/lab6P22.jpg)
+ [part3 Todo List - click the done mark & update status ](img/lab6P23.jpg)
+ [part3 Todo List - add some tasks before click delete](img/lab6P24.jpg)
+ [part3 Todo List - click the delete button for task 6 and task 2](img/lab6P25.jpg)
--- fun.js
+++ fun.js
@@ -17,6 +17,7 @@ function login(){
          // console.log(Pass.value);
     
      var message=document.createElement('div');
+    
      
         if((UserName.value =='admin')&&(Pass.value =='123')){
          console.log('Welcome');
@@ -26,9 +27,59 @@ function login(){
         }else{
          message.innerHTML='<h1 style="text-align:center"> not registered</h1>';
         }
+        
      
         document.body.appendChild(message);
        
      }
  }
-         
+    
+ function push(){
+    var newtask=document.querySelector('#newtask input');
+    var message=document.createElement('div');
+    var tasks=document.querySelector('#tasks');
+    if(newtask.value.length == 0){
+        console.log('Welcome');
+       
+        message.innerHTML='<h1 style="text-align:center"> enter your task name </h1>';
+        document.body.appendChild(message);
+        
+    }
+
+    else{
+       tasks.innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${newtask.value}
+                </span>
+                <button class="add">
+                <i class="fa fa-check" aria-hidden="true"></i>
+                </button>
+                <button class="delete">
+                <i class="fa fa-window-close" aria-hidden="true"></i>
+                </button>
+
+            </div>
+        `;
+
+        var currentTasks = document.querySelectorAll(".delete");
+        for(var i=0; i<currentTasks.length; i++){
+            currentTasks[i].onclick = function(){
+                this.parentNode.remove();
+            };
+        }
+        
+       var currenttasks =document.querySelectorAll('.add');
+         for(var y=0;y<currenttasks.length;y++){
+            currenttasks[y].onclick=function(){
+                var status=document.createElement('p');
+                status.innerHTML='<p>Done</p>';
+                this.parentNode.appendChild(status);
+                document.body.appendChild(this.parentNode);
+            };
+         }
+        
+
+    }
+ }
+
